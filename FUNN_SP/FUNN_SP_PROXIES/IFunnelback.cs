2b7b6c4b37f57126134aef8bb0a4031c13a6ab79
rmorg <rpfmorg@gmail.com>
2012-03-05 12:25:01 +0000

    New interface for pushing to a Funnelback push collection

---
+++ FUNN_SP/FUNN_SP_PROXIES/IFunnelback.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: rpfmorg
+ * Date: 05/03/2012
+ * Time: 12:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace FUNN_SP.FUNN_SP_PROXIES
+{
+	/// <summary>
+	/// Description of Interface1.
+	/// </summary>
+	public interface IFunnelback
+	{
+		void FunnelbackAdd();
+		void FunnelbackDelete();
+		void FunnelbackCommit();
+	}
+}
