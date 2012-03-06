0edd15870ebbdfa6bfe04f2f3f31e96ce11ec372
Michal Piwoni <michal.piwoni@gmail.com>
2012-03-06 06:20:26 -0800

    drawing xml for users

--- FUNN_SP/FUNN_SP_PROXIES/FunnelbackUser.cs
+++ FUNN_SP/FUNN_SP_PROXIES/FunnelbackUser.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using System.Xml.Serialization;
@@ -22,60 +23,116 @@ namespace FUNN_SP_PROXIES
    {
      
            public FunnelbackConfig myfbx { get; set; }
-           public Web ww { get; set; }
            public ClientContext context { get; set; }
-     /// <summary>
-     /// constructor
-     /// </summary>
-           public FunnelbackUser(ClientContext context)
+           
+       // constructor
+
+           public FunnelbackUser(ClientContext context, FunnelbackConfig fbx)
            {
                this.context  = context;
+               this.myfbx = fbx;
 
            }
 
-
+        //methods
      
         public void ListUsers()
         {
             if (this.context != null)
             {
-                GroupCollection collGroup = this.context.Web.SiteGroups;
-                this.context.Load(collGroup);
-                this.context.ExecuteQuery();
+               GroupCollection collGroup = this.context.Web.SiteGroups;
+               this.context.Load(collGroup);
+               this.context.ExecuteQuery();
+
+               using (StreamWriter writer = new StreamWriter(this.myfbx.outputFolder + "\\users.xml"))
+               {
+                   writer.WriteLine("<?xml version='1.0'?>");
+                   writer.WriteLine("<sharepoint>");
+
+
                
-                 //Console.WriteLine(this.context.Web.SiteUserInfoList.);
-                // Console.ReadLine();
-                //Console.WriteLine(collGroup.Count);
-               // Console.ReadKey();
                 foreach (Group group_num in collGroup)
                 {
 
-                    Console.WriteLine(group_num.Id);
+                   // Console.WriteLine(group_num.Id);
                     Group oGroup = collGroup.GetById(group_num.Id);
 
                     UserCollection collUser = oGroup.Users;
 
                     this.context.Load(collUser,
                                 users => users.Include(
+                                    user => user.Id,
                                     user => user.Title,
                                     user => user.LoginName,
                                     user => user.Email));
 
                     this.context.ExecuteQuery();
 
+                    
+
+
+                        
+
                     foreach (User oUser in collUser)
                     {
+
+                  
+                         writer.WriteLine("<user>");
+                            
+                         writer.WriteLine("<id>{0}</id>", oUser.Id);
+                         writer.WriteLine("<group>{0}</group>", group_num.Id);
+                         writer.WriteLine("<group_title>{0}</group_title>", group_num.Title);
+                        writer.WriteLine("<title>{0}</title>", oUser.LoginName);
+                        writer.WriteLine("<loginname>{0}</loginname>", oUser.LoginName);
+                        writer.WriteLine("<email>{0}</email>", oUser.Email);
+                     
+                               
+
+                        writer.WriteLine("</user>");
+
                         Console.WriteLine("User: {0} Login name: {1} Email: {2}",
-                        oUser.Title, oUser.LoginName, oUser.Email);
+                       oUser.Title, oUser.LoginName, oUser.Email);
+                      
                     }
 
+                    
+                   }
+
+                
+
+               writer.WriteLine("</sharepoint>");
+               }
+
+                    //Iterate the owners group
+                Group ownerGroup = this.context.Web.AssociatedOwnerGroup;
+                this.context.Load(ownerGroup);
+                this.context.Load(ownerGroup.Users);
+                this.context.ExecuteQuery();
 
 
-
-
-
-
+                foreach (User ownerUser in ownerGroup.Users)
+                {
+                //Console.WriteLine("User Name: " + ownerUser.Title + " Email: " +
+               // ownerUser.Email + " Login: " + ownerUser.LoginName);
                 }
+
+                /*
+                RoleAssignmentCollection roles = this.context.Web.RoleAssignments;
+                this.context.Load(roles);
+                this.context.ExecuteQuery();
+                foreach (RoleAssignment role in roles)
+                {
+                    this.context.Load(role.Member);
+                    this.context.ExecuteQuery();
+                    Console.WriteLine(role.Member.LoginName);
+                }
+                */
+                
+
+
+
+
+
             
 
 
